850732ee54da7907d18a1882f4cfe437217312ab
Undyne1234 <wesley06@zeelandnet.nl>
2024-07-04 15:40:29 +0200

    added a setup for navbar and footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>WeatherVIS</title>
-
-        @vite(['resources/js/app.js'])
-        @vite(['resources/css/app.css'])
-        <!-- CSS -->
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-
-        <!-- JavaScript -->
-        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-
-    </head>
-
-    <body>
+<x-main>
     <div id="map"></div>
-    </body>
+</x-main>
 
-</html>
